bf718137e1ef0929fc4f49f95b6922a015d0f811
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2022-02-09 20:22:51 -0500

    EscortCivilians v1.1 - Major bugfix
    - This mission will no longer be offered from stations owned by players / alliances.

--- EscortCivilians/data/scripts/entity/missionbulletins.lua
+++ EscortCivilians/data/scripts/entity/missionbulletins.lua
@@ -5,12 +5,18 @@ function MissionBulletins.getPossibleMissions()
 
 	local scripts = EscortCivilians_getPossibleMissions()
 
-	if stationTitle == "Military Outpost" then
+	local _Add = true
+    --Don't add this mission to player / alliance stations.
+    if stationFaction.isPlayer or stationFaction.isAlliance then
+        _Add = false
+    end
+
+	if _Add and stationTitle == "Military Outpost" then
 		table.insert(scripts, {path = "data/scripts/player/missions/escortcivilians.lua", prob = 3})
 	end
 
     --Slightly more likely to show up @ hq / habitats
-    if stationTitle == "${faction} Headquarters" or stationTitle == "Habitat" then
+    if _Add and stationTitle == "${faction} Headquarters" or stationTitle == "Habitat" then
         table.insert(scripts, {path = "data/scripts/player/missions/escortcivilians.lua", prob = 3.5})
     end
 
