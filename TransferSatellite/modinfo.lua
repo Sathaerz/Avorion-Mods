155b16f8f21ac18c2bf83c0af53ec446f6747696
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2022-02-11 22:25:16 -0500

    TransferSatellite v1.0.1 - Modconfig update
    - Adds ESCC dependency
    - ID change
    - Changes dependent Avo version

--- TransferSatellite/modinfo.lua
+++ TransferSatellite/modinfo.lua
@@ -4,7 +4,7 @@ meta =
     -- ID of your mod; Make sure this is unique!
     -- Will be used for identifying the mod in dependency lists
     -- Will be changed to workshop ID (ensuring uniqueness) when you upload the mod to the workshop
-    id = "TransferSatellite",
+    id = "2750680477",
 
     -- Name of your mod; You may want this to be unique, but it's not absolutely necessary.
     -- This is an additional helper attribute for you to easily identify your mod in the Mods() list
@@ -44,7 +44,8 @@ meta =
     --      {id = "OptionalMod", min = "0.2", optional = true}, -- we support OptionalMod optionally, starting at version 0.2
     -- },
     dependencies = {
-
+        {id = "2207469437", min = "1.7.4"},
+        {id = "Avorion", min = "2.0", max = "*.*"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
