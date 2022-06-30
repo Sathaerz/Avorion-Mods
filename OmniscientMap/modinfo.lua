769d311880c2dfda87429ca8fd6ad83d6b99505c
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2022-06-30 17:19:54 -0400

    OmniMap update
    Sets the modinfo to use *.*

--- OmniscientMap/modinfo.lua
+++ OmniscientMap/modinfo.lua
@@ -44,7 +44,7 @@ meta =
     --      {id = "OptionalMod", min = "0.2", optional = true}, -- we support OptionalMod optionally, starting at version 0.2
     -- },
     dependencies = {
-        {id = "Avorion", max = "2.0.11"}
+        {id = "Avorion", max = "*.*"}
     },
 
     -- Set to true if the mod only has to run on the server. Clients will get notified that the mod is running on the server, but they won't download it to themselves
