d92abd988a630ed3c4fc39566804ee6d9c223326
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2022-12-12 20:12:38 -0500

    Eradicate Xsotan 1.0
    - Initial Release

---
+++ EradicateXsotan/data/scripts/entity/missionbulletins.lua
@@ -0,0 +1,17 @@
+local EradicateXsotan_getPossibleMissions = MissionBulletins.getPossibleMissions
+function MissionBulletins.getPossibleMissions()
+	local station = Entity()
+	local stationTitle = station.title
+
+	local scripts = EradicateXsotan_getPossibleMissions()
+
+	if stationTitle == "Military Outpost" then
+		table.insert(scripts, {path = "data/scripts/player/missions/eradicatexsotan.lua", prob = 1})
+	end
+
+	if stationTitle == "Resistance Outpost" then
+		table.insert(scripts, {path = "data/scripts/player/missions/eradicatexsotan.lua", prob = 3})
+	end
+
+	return scripts
+end
