053217679385f186990aa8574b358dc55637b758
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2023-01-18 19:37:37 -0500

    Helix v1.1.3 - Balance Update 3
    - Reduced scaling of turret factory helix cannons by a significant amount.

--- HelixCannon/data/scripts/lib/turretingredients.lua
+++ HelixCannon/data/scripts/lib/turretingredients.lua
@@ -1,11 +1,11 @@
 TurretIngredients[WeaponType.HelixCannon] =
 {
-    {name = "Plasma Cell",          amount = 9,    investable = 6,  minimum = 1,    weaponStat = "damage", investFactor = 1.15 },
-    {name = "High Capacity Lens",   amount = 5,    investable = 3,  minimum = 1,    weaponStat = "damage", investFactor = 0.45 },
-    {name = "Energy Tube",          amount = 3,    investable = 9,  minimum = 1,    weaponStat = "reach", },
+    {name = "Plasma Cell",          amount = 9,    investable = 6,  minimum = 1,    weaponStat = "damage", investFactor = 0.75 },
+    {name = "High Capacity Lens",   amount = 5,    investable = 3,  minimum = 1,    weaponStat = "damage", investFactor = 0.35 },
+    {name = "Energy Tube",          amount = 3,    investable = 9,  minimum = 1,    weaponStat = "reach", investFactor = 0.4 },
     {name = "Conductor",            amount = 6,    investable = 9,  minimum = 1,},
     {name = "Energy Container",     amount = 6,    investable = 9,  minimum = 1,},
-    {name = "Power Unit",           amount = 6,    investable = 5,  minimum = 3,    turretStat = "maxHeat", investFactor = 0.75},
+    {name = "Power Unit",           amount = 6,    investable = 5,  minimum = 3,    turretStat = "maxHeat", investFactor = 0.55},
     {name = "Steel",                amount = 4,    investable = 15, minimum = 3,},
     {name = "Crystal",              amount = 3,    investable = 15, minimum = 1,},
 }
