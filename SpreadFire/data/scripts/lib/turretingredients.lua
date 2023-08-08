8bd55c3e3c709b9bb0e22048c04c59dc1c5ad0b9
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2023-08-08 11:44:53 -0400

    Spreadfire v1.1.3 - Balance Update... 3?
    - Increases investable # of high cap lenses and power units.

--- SpreadFire/data/scripts/lib/turretingredients.lua
+++ SpreadFire/data/scripts/lib/turretingredients.lua
@@ -1,11 +1,11 @@
 TurretIngredients[WeaponType.SpreadFire] =
 {
     {name = "Plasma Cell",          amount = 8,    investable = 4,  minimum = 1,    weaponStat = "damage", investFactor = 0.85 },
-    {name = "High Capacity Lens",   amount = 4,    investable = 2,  minimum = 1,    weaponStat = "damage", investFactor = 0.45 },
+    {name = "High Capacity Lens",   amount = 4,    investable = 3,  minimum = 1,    weaponStat = "damage", investFactor = 0.45 },
     {name = "Energy Tube",          amount = 2,    investable = 6,  minimum = 1,    weaponStat = "reach", investFactor = 0.5 },
     {name = "Conductor",            amount = 5,    investable = 6,  minimum = 1,},
     {name = "Energy Container",     amount = 5,    investable = 6,  minimum = 1,},
-    {name = "Power Unit",           amount = 5,    investable = 3,  minimum = 3,    turretStat = "maxHeat", investFactor = 0.75},
+    {name = "Power Unit",           amount = 5,    investable = 4,  minimum = 3,    turretStat = "maxHeat", investFactor = 0.75},
     {name = "Steel",                amount = 4,    investable = 10, minimum = 3,},
     {name = "Crystal",              amount = 2,    investable = 10, minimum = 1,},
 }
