37e5f5717bf39f3360e07b9c8187b597be511e34
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2024-08-21 10:23:05 -0400

    HelixCannon 1.2 - Bugfix
    - Fixes an issue with the script that prevents these from being made into fighters.

--- HelixCannon/data/scripts/entity/merchants/fighterfactory.lua
+++ HelixCannon/data/scripts/entity/merchants/fighterfactory.lua
@@ -4,8 +4,17 @@ local helixCannon_onCreatePressed = FighterFactory.onCreatePressed
 function FighterFactory.onCreatePressed()
     local inventoryItem = turretSelection.selected
     if inventoryItem then
+        local _Buyer
+        local _Player = Player()
+        local _Ship = Sector():getEntity(_Player.craftIndex)
+        if _Ship and _Ship.factionIndex == _Player.allianceIndex then
+            _Buyer = _Player.alliance
+        else
+            _Buyer = _Player
+        end
+
         local idx = inventoryItem.index
-        local turret = Player():getInventory():find(idx)
+        local turret = _Buyer:getInventory():find(idx)
         local tweapontype = legacyDetectWeaponType(turret)
 
         if tweapontype == WeaponType.HelixCannon then
