55feda68ec61f7035cf017fb1d7a46de17178925
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2024-11-17 18:32:22 -0500

    FusionCannon 1.0.6 - Minor update
    - Gets rid of GameVersion / GameVersion dependent code.

--- FusionCannon/data/scripts/lib/weapongenerator.lua
+++ FusionCannon/data/scripts/lib/weapongenerator.lua
@@ -1,17 +1,12 @@
 function WeaponGenerator.generateFusionCannon(rand, dps, tech, material, rarity)
-    local _Version = GameVersion()
     local weapon = Weapon()
     weapon:setProjectile()
 
     local fireDelay = rand:getFloat(3.75, 4.75)   --Normal cannon is 1.5 to 2.5
     local reach = rand:getFloat(750, 1050)      --Normal cannon is 1100 to 1500
     local damage = dps * fireDelay
-    local speed = rand:getFloat(450, 550)       --Normal cannon is 300 - 400
+    local speed = rand:getFloat(650, 850)       --Normal cannon is 600 - 800
     local existingTime = reach / speed
-
-    if _Version.major > 1 then
-        speed = rand:getFloat(650, 850) --2.0 cannon is 600 - 800
-    end
 
     weapon.fireDelay = fireDelay
     weapon.reach = reach
@@ -48,4 +43,4 @@ function WeaponGenerator.generateFusionCannon(rand, dps, tech, material, rarity)
     return weapon
 end
 
-generatorFunction[WeaponType.FusionCannon              ] = WeaponGenerator.generateFusionCannon
+generatorFunction[WeaponType.FusionCannon] = WeaponGenerator.generateFusionCannon
