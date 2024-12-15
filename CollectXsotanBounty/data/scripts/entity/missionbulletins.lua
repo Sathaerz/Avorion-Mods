0008719a707a844d45dffa500d7165fd7a83faae
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2024-12-15 17:32:16 -0500

    CollectXsotanBounty 1.0 - Initial Release
    - Initial release of the Collect Xsotan Bounty mission

---
+++ CollectXsotanBounty/data/scripts/entity/missionbulletins.lua
@@ -0,0 +1,15 @@
+local CollectXsotanBounty_getPossibleMissions = MissionBulletins.getPossibleMissions
+function MissionBulletins.getPossibleMissions()
+	--print("getting possible missions")
+	local station = Entity()
+	local stationTitle = station.title
+
+	local scripts = CollectXsotanBounty_getPossibleMissions()
+
+	--Unlike other missions, you *can* get this from a player or alliance station.
+	if stationTitle == "Military Outpost" or stationTitle == "Research Station" or stationTitle == "Resistance Outpost" then
+		table.insert(scripts, {path = "data/scripts/player/missions/xsotanbounty.lua", prob = 15})
+	end
+
+	return scripts
+end
