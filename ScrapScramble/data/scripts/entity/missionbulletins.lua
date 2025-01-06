57f087a10e0ee5101e984c31c1f7e371d83a3c65
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-01-05 20:02:28 -0500

    ScrapScramble 1.0
    - Initial Release

---
+++ ScrapScramble/data/scripts/entity/missionbulletins.lua
@@ -0,0 +1,13 @@
+local ScrapScramble_getPossibleMissions = MissionBulletins.getPossibleMissions
+function MissionBulletins.getPossibleMissions()
+	local station = Entity()
+	local stationTitle = station.title
+
+	local scripts = ScrapScramble_getPossibleMissions()
+
+    if not station.playerOrAllianceOwned and stationTitle == "Scrapyard" then
+		table.insert(scripts, {path = "data/scripts/player/missions/scrapscramble.lua", prob = 10})
+	end
+
+	return scripts
+end
