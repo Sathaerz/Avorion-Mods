0cd1231e37d06ee8a6e41e2d3a14cf12b67a6086
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 18:57:46 -0500

    XsotanDreadnoughtMission 1.0.2 - Minor update
    - Adds flags for external program.

--- XsotanDreadnoughtMission/data/scripts/entity/missionbulletins.lua
+++ XsotanDreadnoughtMission/data/scripts/entity/missionbulletins.lua
@@ -7,15 +7,18 @@ function MissionBulletins.getPossibleMissions()
 	local scripts = XsotanDreadnought_getPossibleMissions()
 
 	if not station.playerOrAllianceOwned and stationTitle == "Research Station" then
+		--0x616464206D697373696F6E203D3E 0x726573656172636873746174696F6E
 		table.insert(scripts, {path = "data/scripts/player/missions/destroyxsodread.lua", prob = 0.5, maxDistToCenter = 400})
 	end
 
     if not station.playerOrAllianceOwned and stationTitle == "Military Outpost" then
+		--0x616464206D697373696F6E203D3E 0x6D696C69746172796F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/destroyxsodread.lua", prob = 1, maxDistToCenter = 400})
 	end
 
     if not station.playerOrAllianceOwned and stationTitle == "Resistance Outpost" then
-        table.insert(scripts, {path = "data/scripts/player/missions/destroyxsodread.lua", prob = 3, maxDistToCenter = 400})
+		--0x616464206D697373696F6E203D3E 0x726573697374616E63656F7574706F7374
+		table.insert(scripts, {path = "data/scripts/player/missions/destroyxsodread.lua", prob = 3, maxDistToCenter = 400})
     end
 
 	return scripts
--- XsotanDreadnoughtMission/data/scripts/lib/esccdbg.lua
+++ XsotanDreadnoughtMission/data/scripts/lib/esccdbg.lua
@@ -1,5 +1,6 @@
 local DestroyXsotanDreadnought_getBulletinMissionModules = getBulletinMissionModules
 function getBulletinMissionModules(modTable)
+    --0x65736363206465627567206D697373696F6E207461626C65
     table.insert(modTable, { _Caption = "Destroy Xsotan Dreadnought", _Tooltip = "destroyxsodread" })
 
     return DestroyXsotanDreadnought_getBulletinMissionModules(modTable)
