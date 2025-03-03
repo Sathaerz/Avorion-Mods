2b37995e865f9a3454c136f4bed7f31a433ee48d
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 18:59:39 -0500

    TransferSatellite 1.0.9 - Minor update
    - Adds flags for external program.

--- TransferSatellite/data/scripts/entity/missionbulletins.lua
+++ TransferSatellite/data/scripts/entity/missionbulletins.lua
@@ -8,10 +8,12 @@ function MissionBulletins.getPossibleMissions()
     --Don't add this mission to player / alliance stations since this actually makes a unit similar to the energy suppression satellite. 
 	--We don't want to clutter the player's fleet with this.
 	if not station.playerOrAllianceOwned and stationTitle == "Military Outpost" then
+		--0x616464206D697373696F6E203D3E 0x6D696C69746172796F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/transfersatellite.lua", prob = 2})
 	end
 
     if not station.playerOrAllianceOwned and stationTitle == "Research Station" then
+		--0x616464206D697373696F6E203D3E 0x726573656172636873746174696F6E
 		table.insert(scripts, {path = "data/scripts/player/missions/transfersatellite.lua", prob = 3})
 	end
 
