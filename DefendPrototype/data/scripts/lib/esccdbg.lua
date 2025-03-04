1abd895f55df60e0c6d2b580c64c7d4f9297e715
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:55:01 -0500

    DefendPrototype 1.0.11 - Minor update
    - Adds flags for external program.

--- DefendPrototype/data/scripts/lib/esccdbg.lua
+++ DefendPrototype/data/scripts/lib/esccdbg.lua
@@ -1,5 +1,6 @@
 local DefendPrototype_getBulletinMissionModules = getBulletinMissionModules
 function getBulletinMissionModules(modTable)
+    --0x65736363206465627567206D697373696F6E207461626C65
     table.insert(modTable, { _Caption = "Defend Prototype Battleship", _Tooltip = "defendprototype" })
 
     return DefendPrototype_getBulletinMissionModules(modTable)
--- DefendPrototype/data/scripts/sector/background/radiochatter.lua
+++ DefendPrototype/data/scripts/sector/background/radiochatter.lua
@@ -7,14 +7,20 @@ if onClient() then
 
         --General
         if self.GeneralShipChatter then
+            --0x726164696F206368617474657220616C77617973205354415254
+            --Defend Prototype radio chatter
             table.insert(self.GeneralShipChatter, "The tech used to make those prototypes is tough, but they still can't stand up to Scorchers and Devastators.")
             table.insert(self.GeneralShipChatter, "I've heard that once they're done building it, they basically tear down the whole sector. It's eerie.")
             table.insert(self.GeneralShipChatter, "... she said you should target the Deadshots first. What's a Deadshot?")
             table.insert(self.GeneralShipChatter, "For all the money they spend building those things and hiring mercs, you'd think they'd be better defended.")
             table.insert(self.GeneralShipChatter, "My sister picked up a contract to defend a shipyard last week. She said the attack was ferocious but the pay was great!")
+            --0x726164696F206368617474657220616C7761797320454E44
 
             if random():test(0.25) then
+                --0x726164696F2063686174746572203235706374205354415254
+                --Defend Prototype radio chatter
                 table.insert(self.GeneralShipChatter, "Legit question for any captain. How would you deal with 30-50 angry pirates attacking a shipyard within 3-5 minutes?")
+                --0x726164696F206368617474657220323570637420454E44
             end
         end
     end
