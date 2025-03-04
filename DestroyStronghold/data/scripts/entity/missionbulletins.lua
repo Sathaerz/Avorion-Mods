573e3f52ce7a6808fc1aa93ca005a40c05687dbb
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:57:06 -0500

    v1.0.5 - Minor update
    - Adds flags for external program.

--- DestroyStronghold/data/scripts/entity/missionbulletins.lua
+++ DestroyStronghold/data/scripts/entity/missionbulletins.lua
@@ -6,6 +6,7 @@ function MissionBulletins.getPossibleMissions()
 	local scripts = DestroyStronghold_getPossibleMissions()
 
 	if not station.playerOrAllianceOwned and stationTitle == "Military Outpost" then
+		--0x616464206D697373696F6E203D3E 0x6D696C69746172796F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/destroystronghold.lua", prob = 1.0})
 	end
 
