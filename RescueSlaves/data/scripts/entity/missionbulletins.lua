38016b5193a540f7c1cac7949443ae0fc37d19e7
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:45:08 -0500

    RescueSlaves 1.2.3 - Minor update
    - Add flags for external program.

--- RescueSlaves/data/scripts/entity/missionbulletins.lua
+++ RescueSlaves/data/scripts/entity/missionbulletins.lua
@@ -7,10 +7,12 @@ function MissionBulletins.getPossibleMissions()
 
     --Don't add this mission to player / alliance stations.
 	if not station.playerOrAllianceOwned and stationTitle == "Habitat" then
+		--0x616464206D697373696F6E203D3E 0x68616269746174
 		table.insert(scripts, {path = "data/scripts/player/missions/rescueslaves.lua", prob = 1, minDistToCenter = 25})
 	end
 
     if not station.playerOrAllianceOwned and stationTitle == "Trading Post" then
+		--0x616464206D697373696F6E203D3E 0x74726164696E67706F7374
         table.insert(scripts, {path = "data/scripts/player/missions/rescueslaves.lua", prob = 0.5, minDistToCenter = 25})
     end
 
