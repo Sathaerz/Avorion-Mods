2e06deaf3da5377bb6bd426592bc0818701e0a88
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:58:01 -0500

    EradicateXsotan 1.0.9 - Minor update
    - Adds flags for external program.

--- EradicateXsotan/data/scripts/entity/missionbulletins.lua
+++ EradicateXsotan/data/scripts/entity/missionbulletins.lua
@@ -6,10 +6,12 @@ function MissionBulletins.getPossibleMissions()
 	local scripts = EradicateXsotan_getPossibleMissions()
 
 	if not station.playerOrAllianceOwned and stationTitle == "Military Outpost" then
+		--0x616464206D697373696F6E203D3E 0x6D696C69746172796F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/eradicatexsotan.lua", prob = 1, maxDistToCenter = 500})
 	end
 
 	if not station.playerOrAllianceOwned and stationTitle == "Resistance Outpost" then
+		--0x616464206D697373696F6E203D3E 0x726573697374616E63656F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/eradicatexsotan.lua", prob = 3, maxDistToCenter = 500})
 	end
 
