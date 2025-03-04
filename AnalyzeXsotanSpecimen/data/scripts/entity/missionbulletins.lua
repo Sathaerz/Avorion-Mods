29c2db6841d822fa77c887b955b35e888d7b2018
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:46:35 -0500

    AnalyzeXsotanSpecimen 1.0.3 - Minor update
    - Adds flags for external program.

--- AnalyzeXsotanSpecimen/data/scripts/entity/missionbulletins.lua
+++ AnalyzeXsotanSpecimen/data/scripts/entity/missionbulletins.lua
@@ -7,6 +7,7 @@ function MissionBulletins.getPossibleMissions()
 	local scripts = XsotanSpecimen_getPossibleMissions()
 
 	if not station.playerOrAllianceOwned and (stationTitle == "Research Station" or stationTitle == "Resistance Outpost") then
+		--0x616464206D697373696F6E203D3E 0x726573656172636873746174696F6E 0x726573697374616E63656F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/xsotanspecimen.lua", prob = 3, maxDistToCenter = 500})
 	end
 
