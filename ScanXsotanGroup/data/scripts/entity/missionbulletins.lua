fdc225d31621c80cd24d128713c709a961e7cf19
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 20:55:16 -0500

    ScanXsotanGroup 1.0.6 - Minor update
    - Adds flags for external program.

--- ScanXsotanGroup/data/scripts/entity/missionbulletins.lua
+++ ScanXsotanGroup/data/scripts/entity/missionbulletins.lua
@@ -7,10 +7,12 @@ function MissionBulletins.getPossibleMissions()
 
     --Don't add this mission to player / alliance stations.
 	if not station.playerOrAllianceOwned and stationTitle == "Military Outpost" then
+		--0x616464206D697373696F6E203D3E 0x6D696C69746172796F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/scanxsotangroup.lua", prob = 2, maxDistToCenter = 500})
 	end
 
     if not station.playerOrAllianceOwned and (stationTitle == "Research Station" or stationTitle == "Resistance Outpost") then
+		--0x616464206D697373696F6E203D3E 0x726573656172636873746174696F6E 0x726573697374616E63656F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/scanxsotangroup.lua", prob = 3, maxDistToCenter = 500})
 	end
 
