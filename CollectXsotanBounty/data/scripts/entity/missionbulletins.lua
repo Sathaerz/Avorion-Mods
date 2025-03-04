5bdf8173c80c65b9f5deca7bbd55a9b71815e0d7
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:54:03 -0500

    CollectXsotanBounty 1.0.4 - Minor update
    - Adds flags for external program.

--- CollectXsotanBounty/data/scripts/entity/missionbulletins.lua
+++ CollectXsotanBounty/data/scripts/entity/missionbulletins.lua
@@ -8,6 +8,7 @@ function MissionBulletins.getPossibleMissions()
 
 	--Unlike other missions, you *can* get this from a player or alliance station.
 	if stationTitle == "Military Outpost" or stationTitle == "Research Station" or stationTitle == "Resistance Outpost" then
+		--0x616464206D697373696F6E203D3E 0x616C6C706C61796572 0x6D696C69746172796F7574706F7374 0x726573656172636873746174696F6E 0x726573697374616E63656F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/xsotanbounty.lua", prob = 15, maxDistToCenter = 500})
 	end
 
--- CollectXsotanBounty/data/scripts/lib/esccdbg.lua
+++ CollectXsotanBounty/data/scripts/lib/esccdbg.lua
@@ -1,5 +1,6 @@
 local CollectXsotanBounty_getBulletinMissionModules = getBulletinMissionModules
 function getBulletinMissionModules(modTable)
+    --0x65736363206465627567206D697373696F6E207461626C65
     table.insert(modTable, { _Caption = "Collect Xsotan Bounty", _Tooltip = "xsotanbounty" })
 
     return CollectXsotanBounty_getBulletinMissionModules(modTable)
