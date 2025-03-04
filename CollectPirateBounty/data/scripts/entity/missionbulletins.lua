0f62ea15515f0ec79177c389eb6f401586a669b5
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:52:49 -0500

    CollectPirateBounty 1.6.6 - Minor update
    - Adds flags for external program

--- CollectPirateBounty/data/scripts/entity/missionbulletins.lua
+++ CollectPirateBounty/data/scripts/entity/missionbulletins.lua
@@ -9,6 +9,7 @@ function MissionBulletins.getPossibleMissions()
 	--Apparently they changed the title of smuggler outposts from 1.3.8 to 2.0 but it doesn't actually matter! This is a pretty version-agnostic change.
 	--Unlike other missions, you *can* get this from a player or alliance station.
 	if stationTitle == "Military Outpost" or stationTitle == "Smuggler Hideout" or stationTitle == "Smuggler's Market" then
+		--0x616464206D697373696F6E203D3E 0x616C6C706C61796572 0x6D696C69746172796F7574706F7374 0x736D7567676C65726F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/piratebounty.lua", prob = 15})
 	end
 
