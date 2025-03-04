28a6d066da7ad1d44d3f4356fcb8b389e6a3e0ce
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:00:48 -0500

    ScrapScramble 1.0.5 - Minor update
    - Adds flags for external program.

--- ScrapScramble/data/scripts/entity/missionbulletins.lua
+++ ScrapScramble/data/scripts/entity/missionbulletins.lua
@@ -6,6 +6,7 @@ function MissionBulletins.getPossibleMissions()
 	local scripts = ScrapScramble_getPossibleMissions()
 
     if not station.playerOrAllianceOwned and stationTitle == "Scrapyard" then
+		--0x616464206D697373696F6E203D3E 0x736372617079617264
 		table.insert(scripts, {path = "data/scripts/player/missions/scrapscramble.lua", prob = 3})
 	end
 
--- ScrapScramble/data/scripts/lib/esccdbg.lua
+++ ScrapScramble/data/scripts/lib/esccdbg.lua
@@ -1,5 +1,6 @@
 local ScrapScramble_getBulletinMissionModules = getBulletinMissionModules
 function getBulletinMissionModules(modTable)
+    --0x65736363206465627567206D697373696F6E207461626C65
     table.insert(modTable, { _Caption = "Scrap Scramble", _Tooltip = "scrapscramble" })
 
     return ScrapScramble_getBulletinMissionModules(modTable)
