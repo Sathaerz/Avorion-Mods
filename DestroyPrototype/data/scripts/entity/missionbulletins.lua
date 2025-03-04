4c33686c72c4b0d3b6c488d4e1eae84b1f53462b
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-03 19:55:56 -0500

    DestroyPrototype 1.3.16 - Minor update
    - Adds flags for external program.

--- DestroyPrototype/data/scripts/entity/missionbulletins.lua
+++ DestroyPrototype/data/scripts/entity/missionbulletins.lua
@@ -7,10 +7,12 @@ function MissionBulletins.getPossibleMissions()
 	local scripts = getPossibleMissions_destroyproto()
 
 	if not station.playerOrAllianceOwned and (stationTitle == "Research Station" or stationTitle == "Shipyard") then
+		--0x616464206D697373696F6E203D3E 0x726573656172636873746174696F6E 0x7368697079617264
 		table.insert(scripts, {path = "data/scripts/player/missions/destroyprototype2.lua", prob = 0.5})
 	end
 
 	if not station.playerOrAllianceOwned and stationTitle == "Military Outpost" then
+		--0x616464206D697373696F6E203D3E 0x6D696C69746172796F7574706F7374
 		table.insert(scripts, {path = "data/scripts/player/missions/destroyprototype2.lua", prob = 1.0})
 	end
 
