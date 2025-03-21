ed9cf58df57f9d2d788dd1a77950294433568dc1
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-21 00:26:05 -0400

    TransferSatellite 1.0.10 - Minor update
    - The mission is now automatically tracked when accepting it.

--- TransferSatellite/data/scripts/player/missions/transfersatellite.lua
+++ TransferSatellite/data/scripts/player/missions/transfersatellite.lua
@@ -31,6 +31,7 @@ mission._Name = "Transfer Satellite"
 --Standard mission data.
 mission.data.brief = mission._Name
 mission.data.title = mission._Name
+mission.data.autoTrackMission = true
 mission.data.description = {
     { text = "You recieved the following request from the ${sectorName} ${giverTitle}:" }, --Placeholder
     { text = "..." }, --Placeholder
