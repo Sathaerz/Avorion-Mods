aa53b392492f92f380e7ec523f23909ff1eeedbb
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-21 00:28:07 -0400

    EscortCivilians 1.2.8 - Minor update
    - The mission is now automatically tracked when accepting it.

--- EscortCivilians/data/scripts/player/missions/escortcivilians.lua
+++ EscortCivilians/data/scripts/player/missions/escortcivilians.lua
@@ -47,6 +47,7 @@ mission._Name = "Escort Civilian Transports"
 --Standard mission data.
 mission.data.brief = mission._Name
 mission.data.title = mission._Name
+mission.data.autoTrackMission = true
 mission.data.description = {
     {text = "You recieved the following request from the ${sectorName} ${giverTitle}:" }, --Placeholder
     {text = "..." },
