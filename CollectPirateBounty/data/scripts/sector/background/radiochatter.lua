81c8f5838f2f0bf3cf7d42e3bb3c09cae9bcc8a3
Sathaerz <67401943+Sathaerz@users.noreply.github.com>
2025-03-29 03:44:29 -0400

    CollectPirateBounty 1.6.10 - More yappin
    - Adds more radio chatter

--- CollectPirateBounty/data/scripts/sector/background/radiochatter.lua
+++ CollectPirateBounty/data/scripts/sector/background/radiochatter.lua
@@ -10,6 +10,8 @@ if onClient() then
             --0x726164696F206368617474657220616C77617973205354415254
             --Collect Pirate Bounty radio chatter
             table.insert(self.GeneralShipChatter, "I've heard some pirates will put out a bounty on captains that hunt them.")
+            table.insert(self.GeneralShipChatter, "... I heard some captain whining about constant bounty hunter attacks the other day. I guess he didn't know they'd stop if he abandoned the contract.")
+            table.insert(self.GeneralShipChatter, "Getting paid for killing pirates? If I was gonna do that anyways it's basically free money right?")
             --0x726164696F206368617474657220616C7761797320454E44
         end
     end
